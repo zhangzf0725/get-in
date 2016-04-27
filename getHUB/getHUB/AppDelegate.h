c02f565aa2cabcd21d648b4eb330dc6c2525b01e
zhangzf0725 <1028986378@qq.com>
2016-04-27 16:28:19 +0800

    First
    代码

---
+++ getHUB/getHUB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  getHUB
+//
+//  Created by ZZF on 16-4-27.
+//  Copyright (c) 2016年 zzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ getHUB/getHUB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  getHUB
+//
+//  Created by ZZF on 16-4-27.
+//  Copyright (c) 2016年 zzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
